f6d31f7096b2ec5cb0a3521e564347ac31e452b5
Jan "yenda" Trmal <jtrmal@gmail.com>
2018-06-15 15:09:52 -0400

    avoid clang generating warning about wrong template specialization (#22)

--- Types.cc
+++ Types.cc
@@ -6,7 +6,7 @@
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License Version 2 (June
  * 1991) as published by the Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -28,7 +28,25 @@
 #include "Types.hh"
 
 namespace Core {
-
+  const char *Type<u32>::name("u32");
+  const u32 Type<u32>::max(4294967295U);
+  const u32 Type<u32>::min(0U);
+  const char *Type<s32>::name("s32");
+  const s32 Type<s32>::max(2147483647);
+  const s32 Type<s32>::min(-2147483647 - 1); // gcc warns about too large int when -2147483648
+  const char *Type<u8>::name("u8");
+  const u8  Type<u8 >::max(255U);
+  const u8  Type<u8 >::min(0U);
+  const char *Type<s8>::name("s8");
+  const s8  Type<s8 >::max(127);
+  const s8  Type<s8 >::min(-128);
+  const char *Type<u16>::name("u16");
+  const u16 Type<u16>::max(65535U);
+  const u16 Type<u16>::min(0U);
+  const char *Type<s16>::name("s16");
+  const s16 Type<s16>::max(32767);
+  const s16 Type<s16>::min(-32768);
+#if 0
     template<> const char *Type<u8>::name("u8");
     template<> const u8  Type<u8 >::max(255U);
     template<> const u8  Type<u8 >::min(0U);
@@ -41,37 +59,38 @@ namespace Core {
     template<> const char *Type<s16>::name("s16");
     template<> const s16 Type<s16>::max(32767);
     template<> const s16 Type<s16>::min(-32768);
-    template<> const char *Type<u32>::name("u32");
-    template<> const u32 Type<u32>::max(4294967295U);
-    template<> const u32 Type<u32>::min(0U);
-    template<> const char *Type<s32>::name("s32");
-    template<> const s32 Type<s32>::max(2147483647);
-    template<> const s32 Type<s32>::min(-2147483647 - 1); // gcc warns about too large int when -2147483648
-#if defined(HAS_64BIT)
-    template<> const char *Type<u64>::name("u64");
-    template<> const u64 Type<u64>::max(18446744073709551615U);
-    template<> const u64 Type<u64>::min(0U);
-    template<> const char *Type<s64>::name("s64");
-    template<> const s64 Type<s64>::max(9223372036854775807LL);
-    template<> const s64 Type<s64>::min(-9223372036854775808LL);
+    const char *Type<u32>::name("u32");
+    const u32 Type<u32>::max(4294967295U);
+    const u32 Type<u32>::min(0U);
+    const char *Type<s32>::name("s32");
+    const s32 Type<s32>::max(2147483647);
+    const s32 Type<s32>::min(-2147483647 - 1); // gcc warns about too large int when -2147483648
 #endif
-    template<> const char *Type<f32>::name("f32");
-    template<> const f32 Type<f32>::max(+3.40282347e+38F);
-    template<> const f32 Type<f32>::min(-3.40282347e+38F);
-    template<> const f32 Type<f32>::epsilon(1.19209290e-07F);
-    template<> const f32 Type<f32>::delta(1.17549435e-38F);
-    template<> const char *Type<f64>::name("f64");
-    template<> const f64 Type<f64>::max(+1.7976931348623157e+308);
-    template<> const f64 Type<f64>::min(-1.7976931348623157e+308);
-    template<> const f64 Type<f64>::epsilon(2.2204460492503131e-16);
-    template<> const f64 Type<f64>::delta(2.2250738585072014e-308);
+#if defined(HAS_64BIT)
+    const char *Type<u64>::name("u64");
+    const u64 Type<u64>::max(18446744073709551615U);
+    const u64 Type<u64>::min(0U);
+    const char *Type<s64>::name("s64");
+    const s64 Type<s64>::max(9223372036854775807LL);
+    const s64 Type<s64>::min(-9223372036854775808LL);
+#endif
+    const char *Type<f32>::name("f32");
+    const f32 Type<f32>::max(+3.40282347e+38F);
+    const f32 Type<f32>::min(-3.40282347e+38F);
+    const f32 Type<f32>::epsilon(1.19209290e-07F);
+    const f32 Type<f32>::delta(1.17549435e-38F);
+    const char *Type<f64>::name("f64");
+    const f64 Type<f64>::max(+1.7976931348623157e+308);
+    const f64 Type<f64>::min(-1.7976931348623157e+308);
+    const f64 Type<f64>::epsilon(2.2204460492503131e-16);
+    const f64 Type<f64>::delta(2.2250738585072014e-308);
 
     template <size_t size>
     void swapEndianess(void *buf, size_t count = 1) {
-	char *b = (char*) buf ;
-	for (size_t j = 0 ; j < size / 2 ; ++j)
-	    for (size_t i = 0 ; i < count ; ++i)
-		std::swap(b[i * size + j], b[i * size + size - j - 1]) ;
+      char *b = (char*) buf ;
+      for (size_t j = 0 ; j < size / 2 ; ++j)
+        for (size_t i = 0 ; i < count ; ++i)
+          std::swap(b[i * size + j], b[i * size + size - j - 1]) ;
     }
 
     template void swapEndianess<2>(void *buf, size_t count);
--- Types.hh
+++ Types.hh
@@ -6,7 +6,7 @@
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License Version 2 (June
  * 1991) as published by the Free Software Foundation.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -47,71 +47,153 @@ typedef double      f64;
 
 namespace Core {
 
-    /** Static information about elementary types. */
-    template<class T> struct Type {
+  /** Static information about elementary types. */
+  template<class T> struct Type {
 
-	/** Name to be used to represent data type. */
-	static const char *name;
+    /** Name to be used to represent data type. */
+    static const char *name;
 
-	/** Largest representable value of data type. */
-	static const T max;
-
-	/**
-	 * Smallest representable value of data type.
-	 * Note that unlike std::numeric_limits<>::min this is the most negative
-	 * value also for floating point types.
-	 */
-	static const T min;
-
-	/**
-	 * The difference between the smallest value greater than one and one.
-	 */
-	static const T epsilon;
-
-	/**
-	 * Smallest representable value greater than zero.
-	 * For all integer types this is one.  For floating point
-	 * types this is the same as std::numeric_limits<>::min or
-	 * FLT_MIN / DBL_MIN.
-	 */
-	static const T delta;
-
-    };
+    /** Largest representable value of data type. */
+    static const T max;
 
     /**
-     *  Use this class for naming your basic classes.
-     *  Creating new names: by specialization.
-     *  @see example Matrix.hh
+     * Smallest representable value of data type.
+     * Note that unlike std::numeric_limits<>::min this is the most negative
+     * value also for floating point types.
      */
-    template <typename T>
-    class NameHelper : public std::string {
-    public:
-	NameHelper() : std::string(Type<T>::name) {}
-    };
+    static const T min;
 
-    template <>
-    class NameHelper<std::string> : public std::string {
-    public:
-	NameHelper() : std::string("string") {}
-    };
+    /**
+     * The difference between the smallest value greater than one and one.
+     */
+    static const T epsilon;
 
-    template <>
-    class NameHelper<bool> : public std::string {
-    public:
-	NameHelper() : std::string("bool") {}
-    };
+    /**
+     * Smallest representable value greater than zero.
+     * For all integer types this is one.  For floating point
+     * types this is the same as std::numeric_limits<>::min or
+     * FLT_MIN / DBL_MIN.
+     */
+    static const T delta;
 
-    template <typename T>
-    class NameHelper<std::complex<T> > : public std::string {
-    public:
-	NameHelper() : std::string(std::string("complex-") + NameHelper<T>()) {}
-    };
+  };
 
-    template <typename T>
-    class NameHelper<std::vector<T> > : public std::string {
+  template<> struct Type<f32> {
+    static const char *name;
+    static const f32 max;
+    static const f32 min;
+    static const f32 epsilon;
+    static const f32 delta;
+  };
+
+  template<> struct Type<f64> {
+    static const char *name;
+    static const f64 max;
+    static const f64 min;
+    static const f64 epsilon;
+    static const f64 delta;
+  };
+
+#if defined(HAS_64BIT)
+  template<> struct Type<s64> {
+    static const char *name;
+    static const s64 max;
+    static const s64 min;
+    static const s64 epsilon;
+    static const s64 delta;
+  };
+
+  template<> struct Type<u64> {
+    static const char *name;
+    static const u64 max;
+    static const u64 min;
+    static const u64 epsilon;
+    static const u64 delta;
+  };
+#endif
+
+  template<> struct Type<s32> {
+    static const char *name;
+    static const s32 max;
+    static const s32 min;
+    static const s32 epsilon;
+    static const s32 delta;
+  };
+
+  template<> struct Type<u32> {
+    static const char *name;
+    static const u32 max;
+    static const u32 min;
+    static const u32 epsilon;
+    static const u32 delta;
+  };
+
+  template<> struct Type<s16> {
+    static const char *name;
+    static const s16 max;
+    static const s16 min;
+    static const s16 epsilon;
+    static const s16 delta;
+  };
+
+  template<> struct Type<u16> {
+    static const char *name;
+    static const u16 max;
+    static const u16 min;
+    static const u16 epsilon;
+    static const u16 delta;
+  };
+
+  template<> struct Type<s8> {
+    static const char *name;
+    static const s8 max;
+    static const s8 min;
+    static const s8 epsilon;
+    static const s8 delta;
+  };
+
+  template<> struct Type<u8> {
+    static const char *name;
+    static const u8 max;
+    static const u8 min;
+    static const u8 epsilon;
+    static const u8 delta;
+  };
+
+  /**
+   *  Use this class for naming your basic classes.
+   *  Creating new names: by specialization.
+   *  @see example Matrix.hh
+   */
+  template <typename T>
+  class NameHelper : public std::string {
     public:
-	NameHelper() : std::string(std::string("vector-") + NameHelper<T>()) {}
-    };
+      NameHelper() : std::string(Type<T>::name) {}
+  };
+
+  template <>
+  class NameHelper<std::string> : public std::string {
+    public:
+      NameHelper() : std::string("string") {}
+  };
+
+  template <>
+  class NameHelper<bool> : public std::string {
+    public:
+      NameHelper() : std::string("bool") {}
+  };
+
+  template <typename T>
+  class NameHelper<std::complex<T> > : public std::string {
+    public:
+      NameHelper() : std::string(std::string("complex-") + NameHelper<T>()) {}
+  };
+
+  template <typename T>
+  class NameHelper<std::vector<T> > : public std::string {
+    public:
+      NameHelper() : std::string(std::string("vector-") + NameHelper<T>()) {}
+  };
 
 } // namespace Core
 
