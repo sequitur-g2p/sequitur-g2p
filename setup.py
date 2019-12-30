2940c5c3bf452cf7b9a6099e4872bd8a025ccfd2
Yenda Trmal <jtrmal@gmail.com>
2019-12-30 17:47:12 +0100

    modification to setup pipeline

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ __license__   = """
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License Version 2 (June
 1991) as published by the Free Software Foundation.
- 
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -17,7 +17,7 @@ along with this program; if not, you will find it at
 http://www.gnu.org/licenses/gpl.html, or write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110,
 USA.
- 
+
 Should a provision of no. 9 and 10 of the GNU General Public License
 be invalid or become invalid, a valid provision is deemed to have been
 agreed upon which comes closest to what the parties intended
@@ -26,14 +26,17 @@ negligent actions or intended actions or fraudulent concealment.
 """
 
 import os
+import numpy
 
 from distutils.command.build import build
 from setuptools import setup, Extension
 
-import numpy
+
+VERSION = '1.0a1'
 
 with open('requirements.txt') as fp:
     required = fp.read().splitlines()
+
 
 class CustomBuild(build):
     """Custom build class to swig before handling python modules."""
@@ -44,41 +47,42 @@ class CustomBuild(build):
         ('build_scripts', build.has_scripts)
     ]
 
+
 sequiturExtension = Extension(
     '_sequitur_',
-    language = 'c++',
+    language='c++',
     define_macros=[
-	('MULTIGRAM_SIZE', '4')],
-    sources = [
-	'sequitur.i',
-	'Assertions.cc',
-	'Types.cc',
-	'Utility.cc',
-	'Graph.cc',
-	'Multigram.cc'],
-    depends = [
-	'Assertions.hh',
-	'Graph.hh',
-	'Multigram.hh',
-	'MultigramGraph.hh',
-	'Multigram.hh',
-	'Obstack.hh',
-	'PriorityQueue.hh',
-	'Probability.hh',
-	'Python.hh',
-	'ReferenceCounting.hh',
-	'SequenceModel.hh',
-	'Types.hh',
-	'Utility.hh',
+        ('MULTIGRAM_SIZE', '4')],
+    sources=[
+        'sequitur.i',
+        'Assertions.cc',
+        'Types.cc',
+        'Utility.cc',
+        'Graph.cc',
+        'Multigram.cc'],
+    depends=[
+        'Assertions.hh',
+        'Graph.hh',
+        'Multigram.hh',
+        'MultigramGraph.hh',
+        'Multigram.hh',
+        'Obstack.hh',
+        'PriorityQueue.hh',
+        'Probability.hh',
+        'Python.hh',
+        'ReferenceCounting.hh',
+        'SequenceModel.hh',
+        'Types.hh',
+        'Utility.hh',
         'EditDistance.cc',
         'Estimation.cc',
         'SequenceModel.cc',
         'Translation.cc'],
-    include_dirs = [
-        os.path.join(path, 'core/include') for path in numpy.__path__ ],
-    extra_compile_args = [
-	'-DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION']
-    )
+    include_dirs=[
+        os.path.join(path, 'core/include') for path in numpy.__path__],
+    extra_compile_args=[
+        '-DNPY_NO_DEPRECATED_API=NPY_1_7_API_VERSION']
+)
 
 sequiturModules = [
     'Evaluation',
@@ -96,10 +100,10 @@ sequiturScripts = [
     'g2p.py']
 
 
-#os.system('pyrexc SparseVector.pyx')
-#sparseExtension = Extension('SparseVector', ['SparseVector.c'])
-#os.system('pyrexc IntTuple.pyx')
-#intTupleExtension = Extension('IntTuple', ['IntTuple.c'])
+#  os.system('pyrexc SparseVector.pyx')
+#  sparseExtension = Extension('SparseVector', ['SparseVector.c'])
+#  os.system('pyrexc IntTuple.pyx')
+#  intTupleExtension = Extension('IntTuple', ['IntTuple.c'])
 lmModules = [
     'IterMap',
     'mGramCounts',
@@ -112,12 +116,18 @@ lmScripts = [
 
 
 setup(
-    name        = 'sequitur',
-    version     = '1.0a1',
-    description = 'sequence and joint-sequence modelling tool',
-    author      = 'Maximilian Bisani',
-    cmdclass    = {'build': CustomBuild},
-    install_requires=required,
-    py_modules = sequiturModules,
-    ext_modules = [sequiturExtension],
-    scripts = sequiturScripts)
+    name             = 'sequitur-g2p',
+    packages         = ['sequitur-g2p'],
+    version          = VERSION,
+    license          = 'gpl-2.0',
+    description      = 'sequence and joint-sequence modelling tool for g2p',
+    author           = 'Maximilian Bisani',
+    maintainer       = "Jan 'Yenda' Trmal",
+    maintainer_email = 'jtrmal@gmail.com',
+    url              = 'https://github.com/sequitur-g2p/sequitur-g2p',
+    cmdclass         = {'build': CustomBuild},
+    install_requires = required,
+    py_modules       = sequiturModules,
+    ext_modules      = [sequiturExtension],
+    scripts          = sequiturScripts
+)
---
+++ travis/build_wheels.sh
@@ -0,0 +1,34 @@
+#!/bin/bash                                                                        
+# Copyright (c) 2019, Johns Hopkins University ( Yenda Trmal <jtrmal@gmail.com> )
+# License: Apache 2.0
+
+# Begin configuration section.  
+# End configuration section
+set -e -o pipefail 
+set -o nounset                              # Treat unset variables as an error
+set -x
+
+yum install curl
+curl -o swig-4.0.1.tar.gz \
+  https://netix.dl.sourceforge.net/project/swig/swig/swig-4.0.1/swig-4.0.1.tar.gz 
+
+tar xzf swig-4.0.1.tar.gz
+(cd swig-4.0.1; ./configure --without-pcre && make && make install) || exit 1
+
+# Compile wheels
+for PYBIN in /opt/python/*/bin; do
+    "${PYBIN}/pip" install -r /io/dev-requirements.txt
+    "${PYBIN}/pip" wheel /io/ -w wheelhouse/
+done
+
+# Bundle external shared libraries into the wheels
+for whl in wheelhouse/*.whl; do
+    auditwheel repair "$whl" --plat $PLAT -w /io/wheelhouse/
+done
+
+# Install packages and test
+#for PYBIN in /opt/python/*/bin/; do
+#    "${PYBIN}/pip" install python-manylinux-demo --no-index -f /io/wheelhouse
+#    (cd "$HOME"; "${PYBIN}/nosetests" pymanylinuxdemo)
+#done
+
