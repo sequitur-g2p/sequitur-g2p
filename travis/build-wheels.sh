d832b73004cfde476d2d4ca1b4199bd924ca56ef
Yenda Trmal <jtrmal@gmail.com>
2019-12-31 09:15:46 +0100

    hopefully fixes auditwheel error2

--- travis/build-wheels.sh
+++ travis/build-wheels.sh
@@ -18,7 +18,7 @@ set -x
 
 # Compile wheels
 for PYBIN in /opt/python/*/bin; do
-    make clean
+  (cd /io; make clean)
     "${PYBIN}/pip" install -r /io/dev-requirements.txt
     "${PYBIN}/pip" wheel /io/ -w wheelhouse/
 done
