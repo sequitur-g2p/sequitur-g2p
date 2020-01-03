cb051a2991f4d13f2d4e45370dc86821483f0431
Jan Trmal <jtrmal@gmail.com>
2020-01-03 14:34:53 +0000

    make the script recognizable by windows

---
+++ travis/before-build-windows.bat
@@ -0,0 +1,9 @@
+# Copyright (c) 2019, Johns Hopkins University ( Yenda Trmal <jtrmal@gmail.com> )
+# License: Apache 2.0
+
+dir
+pwd
+pip install -r requirements.txt
+pip install -r dev-requirements.txt
+
+
